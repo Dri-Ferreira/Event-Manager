765401963aa166fcd6039389bbf3c9c14a60b7e6
wamachado <adrianoferreira31@gmail.com>
2024-04-15 15:09:38 -0300

    :construction: start of the event manager project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AttendeesModule } from './modules/attendees/attendees.module';
+import { DatabaseService } from './database/database.service';
 
 @Module({
-  imports: [],
+  imports: [AttendeesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
