35ae4ef3ffb5d650220d3a5cb8cd56a58c25e50e
wamachado <adrianoferreira31@gmail.com>
2024-04-16 10:22:52 -0300

    :construction: creating the event method

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AttendeesModule } from './modules/attendees/attendees.module';
 import { DatabaseModule } from './database/database.modules';
+import { EventModule } from './modules/event/event.module';
 
 @Module({
-  imports: [AttendeesModule, DatabaseModule],
+  imports: [AttendeesModule, DatabaseModule, EventModule],
   controllers: [AppController],
   providers: [],
 })
