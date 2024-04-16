a21bcb317d0d8eb371a7ce3f619448cf3cd93428
wamachado <adrianoferreira31@gmail.com>
2024-04-16 10:27:59 -0300

    :mute: removing default nest controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AttendeesModule } from './modules/attendees/attendees.module';
 import { DatabaseModule } from './database/database.modules';
 import { EventModule } from './modules/event/event.module';
 
 @Module({
   imports: [AttendeesModule, DatabaseModule, EventModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
